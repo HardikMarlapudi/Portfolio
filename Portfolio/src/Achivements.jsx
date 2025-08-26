0e5acca90c777275fe8f1d354bd3a99a9e5c4ece
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-08-26 02:32:56 -0400

    fixed achivements.jsx and Projects.jsx

--- Portfolio/src/Achivements.jsx
+++ Portfolio/src/Achivements.jsx
@@ -3,6 +3,21 @@ import './Achivements.css';
 
 function Achivements() {
 
+    function presidentsAward() {
+        window.location.href = "https://sc.edu/about/offices_and_divisions/registrar/transcripts_and_records/honor_lists/";
+        return;
+    }
+
+    function scholarship() {
+        window.location.href = "https://sc.edu/about/offices_and_divisions/financial_aid/scholarships/scholarships_for_sc_residents/life_scholarship/renewal_requirements/";
+        return;
+    }
+
+    function deansAward() {
+        window.location.href = "https://sc.edu/about/offices_and_divisions/registrar/transcripts_and_records/honor_lists/";
+        return;
+    }
+
     return (
 
         <>
@@ -11,9 +26,9 @@ function Achivements() {
         </div>
 
             <div className="awards">
-            <p>University of South Carolina - President's List (Superlative Award)</p>
-            <p>University of South Carolina - SC Life Scholarship (Superlative Award)</p>
-            <p>University of South Carolina - Dean's List (Superlative Award)</p>
+            <p id="awards" onClick={presidentsAward}>University of South Carolina - President's List (Superlative Award)</p>
+            <p id="awards" onClick={scholarship}>University of South Carolina - SC Life Scholarship (Superlative Award)</p>
+            <p id="awards" onClick={deansAward}>University of South Carolina - Dean's List (Superlative Award)</p>
         </div>
         </>
     )
--- Portfolio/src/Projects.jsx
+++ Portfolio/src/Projects.jsx
@@ -3,32 +3,39 @@ import './Projects.css';
 
 function Projects() {
 
-   const projectCard = document.querySelector(".project-card-view card");
-
-   function URL(url) {
-    window.open(url, '_blank');
-   }
-
-   if(document.querySelector("project-card-view card")) {
-    projectCard.addEventListener('click', () => {
-        URL('https://github.com/HardikMarlapudi/Calculator_React.js');
+   function calculatorProgram() {
+        window.location.href = "https://github.com/HardikMarlapudi/Calculator_React.js";
         return;
-    });
-   }
+    }
+
+   function formsApplication() {
+        window.location.href = "https://github.com/HardikMarlapudi/Forms";
+        return;
+    }
+
+    function BMICalculator() {
+        window.location.href = "https://github.com/HardikMarlapudi/BMI-Calculator";
+        return;
+    }
+
+   function portfolio() {
+        window.location.href = "https://github.com/HardikMarlapudi/Portfolio";
+        return;
+    }
 
     return (
         <>
         <h1 id="projectTitle">My Recent Works</h1>
         <p id="projectIdeas">Here are a few projects I've worked on recently.</p>
 
-        <div className="project-card-view card">
+        <div className="project-card-view card" onClick={calculatorProgram}>
             <p>Calculator program</p>
             <p>A sample calculator program that takes user input for two numbers and an operartor, then performs the corresponding calcuation and displays the result.
                 This program involves conditional (if/else or switch) to handle different opertations and error handling, such as preventing division by zero.
             </p>
         </div>
 
-        <div className="project-card-view card">
+        <div className="project-card-view card" onClick={formsApplication}>
             <p>Forms Application</p>
             <p>A forms application that takes the user inputs for each field and records the users details
                 as the output and displays it in the console and a brand new user can clear the details and
@@ -36,11 +43,15 @@ function Projects() {
             </p>
         </div>
 
-        <div className="project-card-view card">
-            <p>Portfolio</p>
-            <p>Builded a Portfolio using React.js to introducing myself about becoming a professional dev and as an 
-                and a person who loves to code and learn new things as an engninnering student.
+        <div className="project-card-view card" onClick={BMICalculator}>
+            <p>BMI Calculator</p>
+            <p>A BMI calculator that takes the user input for their height and weight and calculates their BMI.</p>
+        </div>
 
+        <div className="project-card-view card" onClick={portfolio}>
+            <p>Portfolio</p>
+            <p>Builded a Portfolio using React.js to introducing myself about becoming a professional dev and as an
+                and a person who loves to code and learn new things as an engninnering student.
             </p>
         </div>
         </>
