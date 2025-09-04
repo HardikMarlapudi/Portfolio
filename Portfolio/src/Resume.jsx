c3900552ae5286a9a15a345faaecca7e1160b4eb
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-09-04 15:59:17 -0400

    fixed resume view link

--- Portfolio/src/Resume.jsx
+++ Portfolio/src/Resume.jsx
@@ -6,10 +6,11 @@ function Resume() {
     return (
         <>
         <h2 id="title">Resume</h2>
-        <a href="https://www.overleaf.com/project/68b247512d81fa16526dd0e3" id="resume" target="_blank">Resume</a>
+        <a href="https://www.overleaf.com/read/wpmtzsvqjttf#f418ee" id="resume" target="_blank">Resume</a>
         <p id="aboutResume">(If you would like to know more about me applying for IT jobs, then please feel free to take a look at my resume.)</p>
         </>
     )
 }
 
 export default Resume;
+kkdkdkdkdkdk
