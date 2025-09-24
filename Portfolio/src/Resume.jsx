ffb3c31b062fe68de420bb6f30c744db68a04c0a
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-09-23 21:01:56 -0400

    fixed resume preview

--- Portfolio/src/Resume.jsx
+++ Portfolio/src/Resume.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {FaHtml5 , FaCss3Alt, FaJsSquare, FaJava, FaReact} from "react-icons/fa";
+import resume from "/pdf/resume.pdf";
 import { IoLogoVue } from "react-icons/io5";
 import { MdOutlineSecurity } from "react-icons/md";
 import './Resume.css';
@@ -11,47 +12,49 @@ function Resume() {
     return (
         <>
         <h2 id="title" className="title fade-in">Resume</h2>
-        <a href="https://www.overleaf.com/read/wpmtzsvqjttf#f418ee" id="resume" className="resume fade-in" target="_blank">Resume</a>
         <p id="aboutResume" className="aboutResume fade-in">If you're interested in my qualifications for IT roles, feel free to download or preview my resume below.</p>
 
         <div className="resume-actions fade-in">
-            <a href="/HardikMarlapudi(resume).pdf" download className="btn download-btn" id="btn-download-btn">Download Resume</a>
-            <button className="btn preview-btn" onClick={() => setShowPreview(!showPreview)}>
-                {showPreview ? "Hide Preview" : "Preview Resume"}
+            <button className="btn-download-btn" onClick={() => setShowPreview(!showPreview)}>
+                {showPreview ? "Hide Preview" : "Show Preview"}
             </button>
         </div>
 
         {showPreview && (
         <div className="resume-preview">
-            <iframe src="https://www.overleaf.com/read/wpmtzsvqjttf#f418ee" frameBorder="0" title="Resume Preview"></iframe>
+            <iframe src={resume} title="Resume Preview"></iframe>
         </div>
         )}
 
         <div className="skills fade-in">
-            <h3>Skills</h3>
+            <h3 id="skills">Skills</h3>
             <ul>
+                <div className="icons">
                 <li><FaHtml5 /><FaCss3Alt/> HTML/CSS</li>
                 <li><FaJsSquare/> JavaScript</li>
                 <li><FaJava/> Java</li>
                 <li><FaReact/> React.js</li>
                 <li><IoLogoVue/> Vue.js</li>
                 <li><MdOutlineSecurity /> CyberSecurity Fundamentals</li>
+                </div>
             </ul>
         </div>
 
         <div className="certifications fade-in">
-            <h2>Certifications</h2>
-            <ul>
-                <li>Google Cybersecurity Certificate (2025)</li>
-                <li>Frontend Mentor Challenges (React)</li>
+            <h2 id="certifications">Certifications</h2>
+            <ul id="certificationList">
+                <p>Google Cybersecurity Certificate (2025)</p>
+                <p>Frontend Mentor Challenges (React)</p>
             </ul>
         </div>
         <div className="why-me fade-in">
-            <h3>Why Me?</h3>
+            <center><h3 id="whyMe">Why Me?</h3></center>
+            <center>
             <p>
                 I'm a passionate developer with a strong foundation in React, JavaScript, and cybersecurity.
                 I love building responsive, interactive UIs and solving real-world problems through clean, sclable code.
             </p>
+            </center>
         </div>
         </>
     )
